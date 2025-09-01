97c07607b0d9d279d8639693ef730f2d53fb1ba9
Juan Baldenegro <132092063+juanBalde2002@users.noreply.github.com>
2025-09-01 20:01:38 -0300

    se creo la clase Table

--- src/Table.cs
+++ src/Table.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
-//hola
+using System.Data;
+
 namespace Ucu.Poo.Restaurant
 {
     /// <summary>
@@ -7,8 +8,38 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Table
     {
+        //definicion de ATRIBUTOS
         private List<Dish> order = new List<Dish>();
-
+        private int Number { get; set; }
+        private bool IsOccupied { get; set; }
+        
+        //definicion de METODOS
+        public Table(int UnNumero, bool NoOcupada = false) //CONSTRUCTOR
+        {
+            this.Number = UnNumero;
+            this.IsOccupied = NoOcupada;
+        }
+        
+        // Marcar la mesa como ocupada.
+        public void Occupy()
+        {
+            this.IsOccupied = true;
+        }
+        
+        //Liberar la mesa y vaciar la lista de pedidos.
+        public void Free()
+        {
+            this.IsOccupied = false;
+            this.order.Clear();
+        }
+        
+        //Agregar un platillo a la lista de pedidos.
+        public void AddToOrder(Dish dish)
+        {
+            this.order.Add(dish);
+        }
+        
+        //Determinar si la mesa tiene pedidos o no; retorna true si los tiene y false en caso contrario.
         public bool HasOrders()
         {
             return this.order.Count > 0;
