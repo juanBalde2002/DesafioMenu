48cd12605f479fb048675f25d2dae0af62c1f19f
joaco <joaquin.garciaq@correo.ucu.edu.uy>
2025-09-02 17:45:13 -0300

    Clase menu terminada Joaco pa las babys

--- src/Menu.cs
+++ src/Menu.cs
@@ -2,12 +2,37 @@ using System.Collections.Generic;
 
 namespace Ucu.Poo.Restaurant
 {
-    /// <summary>
-    /// Representa el conjunto de platillos <see cref="Dish"/> disponibles en el
-    /// restaurante.
-    /// </summary>
     public class Menu
     {
         private List<Dish> dishes = new List<Dish>();
+
+        public void AddDish(Dish plato)
+        {
+            dishes.Add(plato);
+        }
+
+        public void RemoveDish(Dish plato)
+        {
+            dishes.Remove(plato);
+        }
+
+        public Dish GetDishByName(string nombreBuscar)
+        {
+            foreach (Dish plato in dishes)
+            {
+                if (plato.Nombre == nombreBuscar)
+                {
+                    return plato;
+                }
+                
+            }
+
+            return null;
+        }
+
+        public Menu()
+        {
+            dishes = new List<Dish>();
+        }
     }
 }
