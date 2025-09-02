744f43aa5f3b966aad4d6bbb2ee341e5c7fc58ef
SantiCorreaJau <santicorreajau@gmail.com>
2025-09-02 19:00:29 -0300

    Añado un comentario

--- src/Menu.cs
+++ src/Menu.cs
@@ -10,5 +10,27 @@ namespace Ucu.Poo.Restaurant
     {
         
         private List<Dish> dishes = new List<Dish>();
+
+        public void AddDish(Dish plato)
+        {
+         dishes.Add(plato);   
+        }
+
+        public void RemoveDish(Dish plato)
+        {
+            dishes.Remove(plato);
+        }
+
+        public Dish GetDishByName(string nombre)
+        {
+            foreach (Dish p in dishes)
+            {
+                if (nombre == p.Nombre)
+                {
+                    return p;
+                }
+            }  //cambio prueba
+            return null;
+        }
     }
 }
